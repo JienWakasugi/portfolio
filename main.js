6578d46cffda7e9d900d1ff3f0d071f74c074e05
JienWakasugi <gosat20@yahoo.co.jp>
2022-01-03 21:01:43 +0900

    zoom function

--- main.js
+++ main.js
@@ -26,6 +26,31 @@ darkmode.onclick =() =>{
 }
 
 //modal
+const modal = document.querySelector(".modal");
+const preview = document.querySelectorAll(".explain-photos img");
+const original = document.querySelector(".full-img");
+const imgText = document.querySelector(".caption");
+preview.forEach(preview => {
+    preview.addEventListener('click', () =>{
+            modal.classList.add('open');
+            original.classList.add('open');
+
+            const originalSrc = preview.getAttribute("data-original");
+            original.src=`./img/${originalSrc}`;
+            /*console.log(originalSrc);*/
+            const altText = preview.alt;
+            caption.textContent = altText;
+    });
+});
+
+modal.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("modal")){
+        modal.classList.remove('open');
+        original.classList.remove('open');
+    }
+});
+
+
 /*const modalWrapper = document.querySelector('.modal-wrapper');
 const images = document.querySelectorAll('.image');
 const modalImage = document.querySelector('.modal-image');
