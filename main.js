be2b2881afb895e7201e058d82fa83e3a64b07a2
JienWakasugi <gosat20@yahoo.co.jp>
2022-01-09 21:51:04 +0900

    change scroll

--- main.js
+++ main.js
@@ -50,34 +50,111 @@ modal.addEventListener("click", (e) =>{
 });
 
 /*スクロール*/ 
-const scrollElements = document.querySelectorAll(".js-scroll");
-const elementInView = (el, scrollOffset = 0) => {
-    const elementTop = el.getBoundingClientRect().top;
-  
-    return (
-      elementTop <= 
-      ((window.innerHeight || document.documentElement.clientHeight) - scrollOffset)
-    );
-  };
 
-const displayScrollElement = (element) => {
-    element.classList.add("scrolled");
+const scrollElements = document.querySelectorAll(".js-scroll");
+const throttleCount = document.getElementById('throttle-count');
+const scrollCount = document.getElementById('scroll-count');
+
+var throttleTimer;
+
+const throttle = (callback, time) => {
+  if (throttleTimer) return;
+
+	throttleTimer = true;
+	setTimeout(() => {
+    callback();
+		throttleTimer = false;
+	}, time);
+}
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
 };
 
-scrollElements.forEach((el) => {
-    el.style.opacity = 0;
-  });
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
 
-  const handleScrollAnimation = () => {
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
     scrollElements.forEach((el) => {
-      if (elementInView(el, 100)) {
+      if (elementInView(el, 1.25)) {
         displayScrollElement(el);
-      } else {
-        hideScrollElement(el);
+      } else if (elementOutofView(el)) {
+        hideScrollElement(el)
       }
     })
   }
+  var timer=0;
+  var count=0;
+  var scroll = 0;
 
+  window.addEventListener("scroll", () => { 
+    /*scrollCount.innerHTML = */scroll++;
+    throttle(() => {
+      handleScrollAnimation();
+      /*throttleCount.innerHTML = */count++;
+    }, 250);
+  });
+
+/*
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+*/
 
 /*const modalWrapper = document.querySelector('.modal-wrapper');
 const images = document.querySelectorAll('.image');
