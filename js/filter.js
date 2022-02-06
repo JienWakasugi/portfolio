384ba7eb3ada712cecfcd692e2c9638aff0ebc2f
JienWakasugi <gosat20@yahoo.co.jp>
2022-02-06 09:57:34 +0900

    to change filter function

--- js/filter.js
+++ js/filter.js
@@ -1,11 +1,35 @@
+const filterButtons = document.querySelector("#filter-btns").children;
+const items = document.querySelector(".portfolio_nav").children;
+  
+for (let i = 0; i < filterButtons.length; i++) {
+    filterButtons[i].addEventListener("click", function () {
+        for (let j = 0; j < filterButtons.length; j++) {
+            filterButtons[j].classList.remove("active")
+        }
+        this.classList.add("active");
+        const target = this.getAttribute("data-filter")
+  
+        for (let k = 0; k < items.length; k++) {
+            items[k].style.display = "none";
+            if (target == items[k].getAttribute("data-id")) {
+                items[k].style.display = "block";
+            }
+            if (target == "all") {
+                items[k].style.display = "block";
+            }
+        }
+  
+    })
+}
+
 //selecting ul
-const gallery = document.querySelector(".gallery");
+//const gallery = document.querySelector(".gallery");
 
 //select imagbox
-const itemboxes = document.querySelectorAll(".portfolio_content")
-console.log(itemboxes)
+//const itemboxes = document.querySelectorAll(".portfolio_content")
+//console.log(itemboxes)
 
-
+/*
 gallery.addEventListener("click", (e)=>{
     console.log(e)
 
@@ -35,4 +59,4 @@ gallery.addEventListener("click", (e)=>{
 
         })
     }
-})
+})*/
