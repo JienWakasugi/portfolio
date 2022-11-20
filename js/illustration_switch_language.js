a7ea0d161824bf28fbd1a1526a4cd63b216303b0
JienWakasugi <gosat20@yahoo.co.jp>
2022-11-20 19:55:59 +0900

    build artworks site

--- js/illustration_switch_language.js
+++ js/illustration_switch_language.js
@@ -3,10 +3,16 @@ let languages = {
     ja: {
       title: "イラスト",
       subtitle:"デッサンコレクション",
+      introduction:"デッサン作品",
+      explain:"2020年より月4回、教室に通いながら作品を書いています。制作時間は8時間です。カメラの作品は作業時間で24時間かかっています。",
+      duration:"期間 2020-2022",
       },
     en: {
         title: "My recent Illustrations",
         subtitle:"Drawing",
+        introduction:"My artworks",
+        explain:"I attend classes and draw 4 times per month since 2020.Time of this artwork is 8 hours.It took 24 hours to work on the camera artworks.",
+        duration:"Duration 2020-2022",
 
   }
  
